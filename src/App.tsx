19d86633eca2ff1fc7ac7d4cf4c4054ba64f2dc3
Piyushrathoree <meonpath008@gmail.com>
2025-04-10 15:23:23 +0530

    feat: add signup form component and related UI elements; update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-
+import SignupFormDemo from "./components/signup-form-demo";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <SignupFormDemo />
+    </>
   );
 }
+  
